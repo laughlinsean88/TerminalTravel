4a04b3987a93851a16c4d4850f05ac3c93baad54
BocBaBox04 <163660647+BocBaBox04@users.noreply.github.com>
2024-03-16 16:06:24 -0500

    Update InteractionController.gd
    Added the ability for the player to pick/drop items by pressing g. There is a bug that if you move the item quickly while holding it you won't be able to drop it. The reasoning is that in line 17 none of the code inside its if statement will run unless the raycast3d is on an the object, so the only way to drop the object is by having the raycast3d on the object and then click g.

--- Player Controller/Interaction/InteractionController.gd
+++ Player Controller/Interaction/InteractionController.gd
@@ -2,6 +2,14 @@ extends RayCast3D
 
 @onready var interact_prompt_label : Label = get_node("InteractionPrompt")
 
+@onready var hand = $Marker3D
+
+var is_obj_held: bool = false
+ 
+var holding_obj: InteractableObject = null
+
+var hold_power: int = 10
+
 func _process(delta):
 	var object = get_collider()
 	interact_prompt_label.text = ""
@@ -13,4 +21,19 @@ func _process(delta):
 		interact_prompt_label.text = "[E] " + object.interact_prompt
 		
 		if Input.is_action_just_pressed("interact"):
-			object._interact()
+			object._interact() 
+
+		if Input.is_action_just_pressed("pick_up"):
+			if !is_obj_held:
+				holding_obj = object
+				object.global_position = hand.global_position
+				is_obj_held = true
+				print("hold")
+			else:
+				holding_obj = null
+				is_obj_held = false
+				print("drop")
+
+	if holding_obj:
+		holding_obj.set_linear_velocity((hand.global_position - holding_obj.global_position) * hold_power)
+		
