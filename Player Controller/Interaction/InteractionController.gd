2c42805cbd38ebefa05a59695ddee602ddf54bd2
BocBaBox04 <163660647+BocBaBox04@users.noreply.github.com>
2024-03-17 00:03:20 -0500

    Update InteractionController.gd

--- Player Controller/Interaction/InteractionController.gd
+++ Player Controller/Interaction/InteractionController.gd
@@ -4,15 +4,17 @@ extends RayCast3D
 
 @onready var hand = $Marker3D
 
-var is_obj_held: bool = false
+var g_pressed: bool = false
  
 var holding_obj: InteractableObject = null
 
 var hold_power: int = 10
 
+
 func _process(delta):
 	var object = get_collider()
 	interact_prompt_label.text = ""
+	g_pressed = false
 	
 	if object and object is InteractableObject:
 		if object.can_interact == false:
@@ -21,19 +23,21 @@ func _process(delta):
 		interact_prompt_label.text = "[E] " + object.interact_prompt
 		
 		if Input.is_action_just_pressed("interact"):
-			object._interact() 
-
-		if Input.is_action_just_pressed("pick_up"):
-			if !is_obj_held:
-				holding_obj = object
-				object.global_position = hand.global_position
-				is_obj_held = true
-				print("hold")
-			else:
-				holding_obj = null
-				is_obj_held = false
-				print("drop")
+			object._interact()
+		
+		if Input.is_action_just_pressed("pick_up") and !g_pressed:
+			holding_obj = object
+			object.global_position = hand.global_position
+			g_pressed = true
+			print("hold")
 
 	if holding_obj:
 		holding_obj.set_linear_velocity((hand.global_position - holding_obj.global_position) * hold_power)
-		
+	if Input.is_action_just_pressed("pick_up") and !g_pressed:
+		holding_obj = null
+		g_pressed = true
+		print("drop")
+
+
+
+
