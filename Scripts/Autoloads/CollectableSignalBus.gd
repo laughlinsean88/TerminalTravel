31a76d5e55099c5ab14d6a9c3ced606f4196267b
laughlinsean88 <laughlinsean88@gmail.com>
2024-03-18 10:16:06 -0400

    fixes

--- Scripts/Autoloads/CollectableSignalBus.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-signal collected(resource : Resource)
-
-func emit_on_collected(resource: Resource)-> void:
-	collected.emit(resource)
-	
--- Scripts/Autoloads/SignalBus.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-signal set_camera_target(target : CharacterBody2D)
-
-func emit_set_camera_target(target : CharacterBody2D)-> void:
-	print("Setting current target: " + str(target.name))
-	set_camera_target.emit(target)
